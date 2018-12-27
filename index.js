f4aa0c99b3bc1a4c678417cca2e204521eafd095
up_ckz <376641691@qq.com>
2018-12-27 17:49:51 +0800

    ppp

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+const PAGE_ACCESS_TOKEN="EAACmipyPSr0BAKJP5FTgmpucFodSZBNdlY4g8q5amMEjZAxFitFUmxjsq6qcqSxGDC2Tlzk7kQaEKHkT46UtYFJWbXdapkcCCZAygtw54kY52cwaODzpNF4qmexxQgQv6G7UHSwOSxaWqKNDSkSAeAtdsrq2ATQpYdD4QZBEwZBjOsZAmF2Kqr"
 
 // Imports dependencies and set up http server
 const
@@ -7,7 +8,7 @@ const
   app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(1337, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
@@ -24,6 +25,14 @@ app.post('/webhook', (req, res) => {
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let flag = ("sender" in webhook_event);
+      if (flag == true) {
+         let sender_psid = webhook_event.sender.id;
+         console.log('Sender PSID: ' + sender_psid);
+      }
+
     });
 
     // Returns a '200 OK' response to all requests
@@ -62,3 +71,19 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
+
+// Handles messages events
+ function handleMessage(sender_psid, received_message) {
+
+ }
+
+ // Handles messaging_postbacks events
+ function handlePostback(sender_psid, received_postback) {
+
+ }
+
+ // Sends response messages via the Send API
+ function callSendAPI(sender_psid, response) {
+   
+   }
+
